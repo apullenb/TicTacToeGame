677e672dbd4b9acd5a3b0273d7e048bfb2c10b2a
Andrea Pullen <70964079+apullenb@users.noreply.github.com>
2021-08-28 16:45:49 -0500

    Create Game

--- Program.cs
+++ Program.cs
@@ -12,11 +12,68 @@ namespace TicTacToeGame
         };
         static void Main(string[] args)
         {
-            SetField();
+            int player = 2;
+            int input = 0;
+            bool inputCorrect = true;
+            
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXOrO(player, input);
+                }
+
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXOrO(player, input);
+                }
+
+                SetField();
+
+                #region
+                // Check winning conditions
+                char[] playerChars = { 'X', 'O' };
+
+                foreach (char playerChar in playerChars)
+                {
+                    if (((playField[0, 0] == playerChar) && (playField[0, 1] == playerChar) && (playField[0, 2] == playerChar))
+                        || ((playField[1, 0] == playerChar) && (playField[1, 1] == playerChar) && (playField[1, 2] == playerChar))
+                        || ((playField[2, 0] == playerChar) && (playField[2, 1] == playerChar) && (playField[2, 2] == playerChar))
+                        || ((playField[0, 0] == playerChar) && (playField[1, 0] == playerChar) && (playField[2, 0] == playerChar))
+                        || ((playField[0, 0] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 2] == playerChar))
+                        || ((playField[0, 2] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 0] == playerChar))
+                        || ((playField[0, 1] == playerChar) && (playField[1, 1] == playerChar) && (playField[2, 1] == playerChar))
+                        || ((playField[0, 2] == playerChar) && (playField[1, 2] == playerChar) && (playField[2, 2] == playerChar))
+                        )
+                    {
+                        if (playerChar == 'X')
+                        {
+                            Console.WriteLine("\nPlayer 2 is the Winner!!!");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("\nPlayer 1 is the Winner!!!");
+                        }
+                        break;
+                    }
+                }
+                #endregion
+                do
+                {
+                    Console.Write("\nPlayer {0}: Choose Your field! ", player);
+                    input = Convert.ToInt32(Console.ReadLine());
+                    
+
+                } while (!inputCorrect);
+            } while (true);
         }
 
         public static void SetField()
         {
+            Console.Clear();
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  {0}  |  {1}  |  {2} ", playField[0,0], playField[0, 1], playField[0, 2]);
             Console.WriteLine(" ____|_____|_____");
@@ -28,6 +85,29 @@ namespace TicTacToeGame
             Console.WriteLine(" ____|_____|_____");
             Console.WriteLine("     |     |     ");
             
+
+        }
+
+        public static void EnterXOrO(int player, int input)
+        {
+            char playerSign = ' ';
+            if (player == 1)
+                playerSign = 'X';
+            else if (player == 2)
+                playerSign = 'O';
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+
+            }
 
         }
     }
