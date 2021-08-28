3d0851ab60c2bc834cb540e1c023439a47909222
Andrea Pullen <70964079+apullenb@users.noreply.github.com>
2021-08-28 15:34:39 -0500

    Create Game Field

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace TicTacToeGame
 {
     class Program
     {
+        static char[,] playField =
+        {
+            {'1', '2', '3' },
+            {'4', '5', '6' },
+            {'7', '8', '9' }
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SetField();
+        }
+
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2} ", playField[0,0], playField[0, 1], playField[0, 2]);
+            Console.WriteLine(" ____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2} ", playField[1, 0], playField[1, 1], playField[1, 2]);
+            Console.WriteLine(" ____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2} ", playField[2, 0], playField[2, 1], playField[2, 2]);
+            Console.WriteLine(" ____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            
+
         }
     }
 }
